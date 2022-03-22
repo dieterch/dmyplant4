9baffd931743a78073be54f2e6719c2b111d190c
DieterChvatal <dieter.chvatal@innio.com>
2022-03-22 18:50:09 +0100

    calcBMEP(Pel,speed) added to engine

--- dmyplant2/dEngine.py
+++ dmyplant2/dEngine.py
@@ -1460,6 +1460,9 @@ class Engine:
     def BMEP(self):
         return np.around(1200.0 * self.Pmech_nominal / (self.engvol * self.Speed_nominal), decimals=1)
 
+    def _calc_BMEP(self, p_Pel, p_Speed):
+        return 1200.0 * (p_Pel / self.Generator_Efficiency) / (self.engvol * p_Speed)
+
     @ property
     def dash(self):
         _dash = dict()
