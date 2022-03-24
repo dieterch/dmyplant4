bfff0ff7b0d725fe2a07a744707d16629fe4ced3
dieterchvatal <dieter.chvatal@gmail.com>
2022-03-24 08:23:07 +0100

    testcode added to dMyPlant

--- dmyplant2/dMyplant.py
+++ dmyplant2/dMyplant.py
@@ -55,7 +55,7 @@ def mp_ts(ts) -> int:
         else:
             return int(ts * 1000.0)
     except:
-        return 0.0
+        return 0
 
 class MyPlantException(Exception):
     pass
@@ -560,3 +560,17 @@ class MyPlant:
         # }
         return pd.DataFrame(val_dict)
 
+if __name__ == "__main__":
+    
+    import dmyplant2
+    from pprint import pprint as pp
+    import time
+    t0 = time.time()
+    dmyplant2.cred()
+    mp = dmyplant2.MyPlant(0)
+    #dmyplant2.Engine._list_cached_validations()
+    t1 = time.time()
+    e = dmyplant2.Engine.from_sn(mp, '1486144')
+    t2 = time.time()
+    pp(e.dash)
+    print(f"Login: {(t1-t0):3.2f}sec\nEngine: {(t2-t1):3.2}sec")
