cf209833a4b51e04c3808401fc232532201e5609
dieterchvatal <dieter.chvatal@gmail.com>
2022-04-19 08:30:05 +0200

    bug fix in new code

--- dmyplant2/dFSM/dFSMData.py
+++ dmyplant2/dFSM/dFSMData.py
@@ -59,9 +59,9 @@ def _resample_data(fsm, data, startversuch):
         odata3 = data[data.datetime >= d3]
     return pd.concat([odata1,odata2,odata3]).reset_index(drop='index')
 
-def get_cycle_data(fsm,startversuch, max_length=None, min_length=None, cycletime=None, silent=False, p_data=None, reduce=True, pre_period=5*60, post_period=21*60, time_range=(0,100)):
-    tns = pd.to_datetime((startversuch['starttime'].timestamp() + time_range.value[0]/100.0 * (startversuch['endtime']-startversuch['starttime']).seconds), unit='s')
-    tne = pd.to_datetime((startversuch['starttime'].timestamp() + time_range.value[1]/100.0 * (startversuch['endtime']-startversuch['starttime']).seconds), unit='s')
+def get_cycle_data(fsm,startversuch, max_length=None, min_length=None, cycletime=None, silent=False, p_data=None, reduce=True, pre_period=5*60, post_period=21*60, t_range=(0,100)):
+    tns = pd.to_datetime((startversuch['starttime'].timestamp() + t_range.value[0]/100.0 * (startversuch['endtime']-startversuch['starttime']).seconds), unit='s')
+    tne = pd.to_datetime((startversuch['starttime'].timestamp() + t_range.value[1]/100.0 * (startversuch['endtime']-startversuch['starttime']).seconds), unit='s')
     t0 = int(tns.timestamp() * 1000 - pre_period * 1000)
     t1 = int(tne.timestamp() * 1000 + post_period * 1000)
     if max_length:
@@ -110,9 +110,9 @@ def _load_reduced_data(fsm, startversuch, ptts_from, ptts_to, pdata=None):
     #_debug(d3t,ptts_to,data3, 'data3')
     return pd.concat([data1,data2,data3]).reset_index(drop='index')
 
-def get_cycle_data2(fsm,startversuch, max_length=None, min_length=None, cycletime=None, silent=False, p_data=None, pre_period=5*60, post_period=21*60, time_range=(0,100)):
-    tns = pd.to_datetime((startversuch['starttime'].timestamp() + time_range.value[0]/100.0 * (startversuch['endtime']-startversuch['starttime']).seconds), unit='s')
-    tne = pd.to_datetime((startversuch['starttime'].timestamp() + time_range.value[1]/100.0 * (startversuch['endtime']-startversuch['starttime']).seconds), unit='s')
+def get_cycle_data2(fsm,startversuch, max_length=None, min_length=None, cycletime=None, silent=False, p_data=None, pre_period=5*60, post_period=21*60, t_range=(0,100)):
+    tns = pd.to_datetime((startversuch['starttime'].timestamp() + t_range.value[0]/100.0 * (startversuch['endtime']-startversuch['starttime']).seconds), unit='s')
+    tne = pd.to_datetime((startversuch['starttime'].timestamp() + t_range.value[1]/100.0 * (startversuch['endtime']-startversuch['starttime']).seconds), unit='s')
     t0 = int(tns.timestamp() * 1000 - pre_period * 1000)
     t1 = int(tne.timestamp() * 1000 + post_period * 1000)
     if max_length:
